612c995178fa95c2e5c0950ba9faf5e1cce93617
anastasiiya3 <porfirianu.anastasiia@chnu.edu.ua>
2025-05-06 09:29:10 +0300

    add int main

--- sotr-insert.cpp
+++ sotr-insert.cpp
@@ -14,3 +14,23 @@ void insertionSort(int arr[], int n) {
         arr[j + 1] = key;
     }
 }
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Початковий масив: ";
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    cout << "Відсортований масив: ";
+    printArray(arr, n);
+
+    return 0;
+}
